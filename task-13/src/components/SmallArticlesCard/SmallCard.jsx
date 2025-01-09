6939ee60d57cf3ba3c322bc5377faf8ac58a0504
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2025-01-09 23:15:19 +0400

    done

--- task-13/src/components/SmallArticlesCard/SmallCard.jsx
+++ task-13/src/components/SmallArticlesCard/SmallCard.jsx
@@ -8,13 +8,13 @@ const SmallCard = ({ title, author, date }) => {
           </p>
         </div>
         <div>
-          <ul className="flex flex-wrap  items-start gap-2 sm:flex-row sm:items-center sm:justify-between">
+          <ul className="flex flex-wrap  items-center gap-2 sm:flex-row sm:items-center sm:justify-between">
             <li>
               <span className="text-[12px] sm:text-[14px] text-[#718096] font-semibold">
                 {author}
               </span>
             </li>
-            <li className="text-[12px] sm:text-[14px] text-[#a0aec0] font-light">
+            <li className="text-[12px] mt-0 sm:text-[14px] text-[#a0aec0] font-light">
               {date} <span className="font-semibold">&#183; </span>
               <i className="ri-timer-2-line font-semibold"></i> 1 min
             </li>
